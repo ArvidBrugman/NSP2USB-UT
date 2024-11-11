58a23d57873c673ef5d14d9c334f9913bea1c82b
ArvidBrugman <brugman.arvid@gmail.com>
2024-11-11 12:16:54 +0100

    csv file created

--- scan.py
+++ scan.py
@@ -6,13 +6,20 @@ amplitude = []
 
 for d in range(0, 215, 5):
     #5.000000
-    distance.append(d)
+    for _ in range(2000):
+        distance.append(d)
     with open(f'{d}.000000.txt') as file:
         for line in file:
             column = line.split()
             time.append(column[0])
             amplitude.append(column[1])
 
-print(time)
-aantal = time.count('0.416480')
-print(aantal)
+print(len(time))
+print(len(amplitude))
+print(len(distance))
+
+with open('measurements.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Distance(mm)", "Time(ms)", "Amptitude(mV)"])
+    for d, t, a in zip(distance, time, amplitude):
+        writer.writerow([d, t, a])
