832720be0fd74c9cd0da9d8280cb568a441f560a
ArvidBrugman <brugman.arvid@gmail.com>
2024-11-18 09:57:56 +0100

    attempts at plotting
    very much failed

--- scan.py
+++ scan.py
@@ -1,6 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
-
+import seaborn as sns
+import numpy as np
+import scipy.interpolate as griddata
 distance = []
 time = []
 amplitude = []
@@ -21,6 +23,33 @@ with open('measurements.csv', 'w', newline='') as csvfile:
     for d, t, a in zip(distance, time, amplitude):
         writer.writerow([d, t, a])
 
+# fig, ax = plt.subplots()
+# amplitude_max = amplitude[0]
+# amplitude_min = amplitude[0]
+# for _ in amplitude:
+#     if _ > amplitude_max:
+#         _ = amplitude_max
+#     if _< amplitude_min:
+#         _ = amplitude_min
 
-plt.scatter(time, distance)
+# c = ax.pcolormesh(time, distance, amplitude, cmap='RdBu', vmin=amplitude_min, vmax=amplitude_max)
+# ax.set_title('pcolormesh')
+# # set the limits of the plot to the limits of the data
+# ax.axis([time.min(), time.max(), distance.min(), distance.max()])
+# fig.colorbar(c, ax=ax)
+
+# plt.show()
+
+x = np.linspace(min(time), max(time), 86001)
+y = np.linspace(min(distance), max(distance), 86001)
+x, y = np.meshgrid(x, y)
+
+z = griddata((time, distance), amplitude, (x, y), method = 'cubic')
+
+print(z)
+sns.heatmap(z)
 plt.show()
+
+# plt.imshow(csvfile, cmap='viridis')
+# plt.colorbar()
+# plt.show()
