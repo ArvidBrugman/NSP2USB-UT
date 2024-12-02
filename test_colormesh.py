68aa4fcda5b903ee3878cc276981c45cacd640cc
ArvidBrugman <dominiquedeboer@yahoo.nl>
2024-12-02 09:37:00 +0100

    x-axis converted into distance

--- test_colormesh.py
+++ test_colormesh.py
@@ -4,7 +4,7 @@ from scipy.signal import hilbert
 import pandas as pd
 
 # Empty lists
-distance = []
+distance_motor = []
 time = []
 amplitude_raw = []
 
@@ -12,7 +12,7 @@ amplitude_raw = []
 # Load data from files
 for d in range(0, 215, 5):
     for _ in range(2000):  # Repeat distances = 800
-        distance.append(d)
+        distance_motor.append(d)
     with open(f'{d}.000000.txt') as file:
         for line in file:
             column = line.split()
@@ -24,26 +24,31 @@ for d in range(0, 215, 5):
 print(len(np.arange(0, 213, 3)))
 print(len(time))
 
+
+distance_reflection = []
+for t in time:
+    distance_reflection.append(t * 1500) #ms * mm/ms
+
 # # Hilbert transform for amplitude envelope
 hilbert_amplitude = hilbert(amplitude_raw)
 amplitude_envelope = np.abs(hilbert_amplitude)
 
 # Create DataFrame
-df = pd.DataFrame({'distance': distance, 'time': time, 'amplitude_envelope': amplitude_envelope})
+df = pd.DataFrame({'distance_motor': distance_motor, 'distance_reflection': distance_reflection, 'amplitude_envelope': amplitude_envelope})
 
 # Pivot data for 2D grid
-pivot_table = df.pivot_table(values='amplitude_envelope', index='time', columns='distance')
+pivot_table = df.pivot_table(values='amplitude_envelope', index='distance_reflection', columns='distance_motor')
 
 # Extract 2D arrays
-distance_values = pivot_table.columns.values  # X-axis
-time_values = pivot_table.index.values  # Y-axis
+distance_motor_values = pivot_table.columns.values  # X-axis
+distance_reflection_values = pivot_table.index.values  # Y-axis
 amplitude_values = pivot_table.values  # Z-axis
 
 # Plot with pcolormesh
 fig, ax = plt.subplots(figsize=(10, 6))
-mesh = ax.pcolormesh(time_values, distance_values, amplitude_values.T, shading='auto', cmap='viridis')
+mesh = ax.pcolormesh(distance_reflection_values, distance_motor_values, amplitude_values.T, shading='auto', cmap='viridis')
 fig.colorbar(mesh, ax=ax, label='Amplitude Envelope (in mV)')
-ax.set_xlabel('Time (in ms)')
-ax.set_ylabel('Distance (in mm)')
+ax.set_xlabel('Scanlength  (in mm)')
+ax.set_ylabel('Distance of motor (in mm)')
 ax.set_title('Amplitude Envelope over Distance and Time')
 plt.show()
