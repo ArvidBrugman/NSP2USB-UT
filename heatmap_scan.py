81714c7b1d7d2bb405b6ac85d13a4ac95b82fb7b
ArvidBrugman <dominiquedeboer@yahoo.nl>
2024-12-04 11:27:28 +0100

    delete measurement and added the script to calculate errors with the values of each measurement

--- heatmap_scan.py
+++ heatmap_scan.py
@@ -10,10 +10,11 @@ from scipy.signal import hilbert
 class heatmap_scan:
     def __init__(self):
         # Empty lists
-        self.distance_motor = []
-        self.amplitude_raw = []
-        self.scanlength = []
-        self.amplitude_around0 = []
+
+        self.amplitude = []
+        self.avg_scanlength = []
+        self.avg_amplitude_envelope = []
+        self.avg_distance_motor = []
 
     def lists(self, stepsize):
         """Create lists of the data gotten from the scan in txt files
@@ -28,36 +29,53 @@ class heatmap_scan:
         """
 
         for d in range(0, 210 + stepsize, stepsize):
-            with open(f"{d}.000000.txt") as file:
-                for line in file:
-                    column = line.split()
-                    self.scanlength.append(float(column[0]) * 1500)  # Convert to float
-                    self.amplitude_raw.append(float(column[1]))  # Convert to float
-                    self.distance_motor.append(d)
+            for meas in range(1, 6):
+                with open(f"{d}.00000{meas}.txt") as file:
+                    for line in file:
+                        self.distance_motor = []
+                        self.amplitude_raw = []
+                        self.scanlength = []
+                        column = line.split()
 
-        mean_amplitude_raw = np.mean(self.amplitude_raw)
+                        self.scanlength.append(
+                            float(column[0]) * 1500
+                        )  # Convert to float
+                        self.amplitude_raw.append(float(column[1]))
+                        # hilbert transformation of self.amplitude_raw
+                        hilbert_amplitude = hilbert(self.amplitude_around0)
+                        self.amplitude_envelope = np.abs(
+                            hilbert_amplitude
+                        )  # Convert to float
+                        self.distance_motor.append(d)
+                        self.avg_scanlength.append(np.mean(self.scanlength))
+                        self.avg_amplitude_envelope.append(
+                            np.mean(self.amplitude_envelope)
+                        )
+                        self.avg_distance_motor.append(np.mean(self.distance_motor))
 
-        for i in self.amplitude_raw:
-            self.amplitude_around0.append(i - mean_amplitude_raw)
+        # mean_amplitude_raw = np.mean(self.amplitude_raw)
 
-        # hilbert transformation of self.amplitude_raw
-        hilbert_amplitude = hilbert(self.amplitude_around0)
-        self.amplitude_envelope = np.abs(hilbert_amplitude)
+        # for i in self.amplitude_raw:
+        #     self.amplitude_around0.append(i - mean_amplitude_raw)
+
+        mean_ampl = np.mean(self.avg_amplitude_envelope)
+        for i in self.avg_amplitude_envelope:
+            self.amplitude.append(i - mean_ampl)
 
     def heatmap(self):
         """Plot the heatmap using pcolormesh"""
         # Create DataFrame
         df = pd.DataFrame(
             {
-                "self.distance_motor": self.distance_motor,
-                "scanlength": self.scanlength,
-                "amplitude_envelope": self.amplitude_envelope,
+                "self.distance_motor": self.avg_distance_motor,
+                "scanlength": self.avg_scanlength,
+                "amplitude": self.amplitude,
             }
         )
-
+        print(df)
         # Pivot data for 2D grid
         pivot_table = df.pivot_table(
-            values="amplitude_envelope",
+            values="amplitude",
             index="scanlength",
             columns="self.distance_motor",
         )
@@ -76,10 +94,10 @@ class heatmap_scan:
             shading="gouraud",
             cmap="seismic",
         )
-        fig.colorbar(mesh, ax=ax, label="Amplitude Envelope (in mV)")
+        fig.colorbar(mesh, ax=ax, label="Amplitude (in mV)")
         ax.set_xlabel("Scanlength  (in mm)")
         ax.set_ylabel("Distance of motor (in mm)")
-        ax.set_title("Amplitude Envelope over Distance and Time")
+        ax.set_title("Amplitude over Distance and Time")
         plt.show()
 
     def csv_creator(self):
@@ -87,9 +105,7 @@ class heatmap_scan:
         with open("raw_values.csv", "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["Distance(mm)", "Time(ms)", "Amptitude(mV)"])
-            for d, t, a in zip(
-                self.distance_motor, self.scanlength, self.amplitude_envelope
-            ):
+            for d, t, a in zip(self.distance_motor, self.scanlength, self.amplitude):
                 writer.writerow([d, t, a])
 
 
